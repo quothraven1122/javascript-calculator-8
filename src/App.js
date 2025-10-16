0e75745b2e793d7b07c434009b2a2be87f549f11
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2025-10-16 14:01:52 +0900

    feat: error handling
    - Use split instead of index just in case custom delimiters are longer than one character
    - Check for presence of "\\n" when parsing custom delimiters to prevent split errors
    - Validate that all input numbers are actual numbers
    - Use try/catch and return to handle errors and print messages without using process.exit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,37 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const input = await Console.readLineAsync(
-      "덧셈할 문자열을 입력해 주세요.\n"
-    );
-    let numbers;
+    try {
+      const input = await Console.readLineAsync(
+        "덧셈할 문자열을 입력해 주세요.\n"
+      );
+      let numbers;
 
-    if (input.startsWith("//")) {
-      //custom delimiter 사용
-      // "//" 다음 char이 구분자
-      const customDelimiter = input[2];
-      // "\n"으로 split하면 왼쪽이 "//{delimiter}", 오른쪽이 숫자 리스트가 된다
-      const numbersWithCustomDelimiter = input.split("\\n")[1];
+      if (input.startsWith("//") && input.includes("\\n")) {
+        //custom delimiter 사용
 
-      numbers = numbersWithCustomDelimiter.split(customDelimiter);
-    } else {
-      //default delimiter 사용
-      numbers = input.split(/[,|:]/);
+        // "\n"으로 split하면 왼쪽이 구분자, 오른쪽이 숫자 리스트가 된다
+        const [customDelimiter, numbersWithCustomDelimiter] = input
+          .slice(2)
+          .split("\\n");
+
+        numbers = numbersWithCustomDelimiter.split(customDelimiter);
+      } else {
+        //default delimiter 사용
+        numbers = input.split(/[, :]/);
+      }
+
+      //numbers 안의 모든 element가 실제로 number인지 확인
+      numbers.forEach((i) => {
+        if (isNaN(Number(i))) {
+          throw new Error("[ERROR] input error");
+        }
+      });
+      Console.print(numbers);
+    } catch (e) {
+      Console.print(e.message);
+      return;
     }
-
-    Console.print(numbers);
   }
 }
 
