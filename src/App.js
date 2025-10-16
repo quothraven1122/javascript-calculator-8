f3bba414a95abdacbb60f4d799234703af3b3144
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2025-10-16 12:14:28 +0900

    feat: support custom delimiter for input
    - Recognize custom delimiter when input starts with "//"
    - Split and extract the actual number string after the delimiter declaration
    - Split the numbers using the custom delimiter
    - Default to comma or colon if no custom delimiter is provided

--- src/App.js
+++ src/App.js
@@ -5,7 +5,21 @@ class App {
     const input = await Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요.\n"
     );
-    const numbers = input.split(/[,|:]/);
+    let numbers;
+
+    if (input.startsWith("//")) {
+      //custom delimiter 사용
+      // "//" 다음 char이 구분자
+      const customDelimiter = input[2];
+      // "\n"으로 split하면 왼쪽이 "//{delimiter}", 오른쪽이 숫자 리스트가 된다
+      const numbersWithCustomDelimiter = input.split("\\n")[1];
+
+      numbers = numbersWithCustomDelimiter.split(customDelimiter);
+    } else {
+      //default delimiter 사용
+      numbers = input.split(/[,|:]/);
+    }
+
     Console.print(numbers);
   }
 }
